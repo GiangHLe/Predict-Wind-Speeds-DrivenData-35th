4c999ba870c7fc67efeeca6542df7463f723c42e
giang <lhgiang149@gmail.com>
2020-12-23 18:47:01 +0900

    for back up

---
+++ old_code/dataset.py
@@ -0,0 +1,106 @@
+import os
+import cv2
+import numpy as np
+import pandas as pd
+import albumentations
+import torch
+from torch.utils.data import Dataset
+
+dataroot = '/home/giang/Desktop/Wind_data/train/'
+# dataroot = '/home/giang/Desktop/Wind_data/test/'
+# dataroot = 'C:/Users/Admin/Desktop/Wind_data/train/'
+
+def get_transforms(image_size, gray = False):
+
+    transforms_train = albumentations.Compose([
+        albumentations.Transpose(p=0.5),
+        albumentations.VerticalFlip(p=0.5),
+        albumentations.HorizontalFlip(p=0.5),
+        albumentations.RandomBrightness(limit=0.2, p=0.75),
+        albumentations.RandomContrast(limit=0.2, p=0.75),
+        albumentations.OneOf([
+            albumentations.MotionBlur(blur_limit=5),
+            albumentations.MedianBlur(blur_limit=5),
+            albumentations.GaussianBlur(blur_limit=5),
+            albumentations.GaussNoise(var_limit=(5.0, 30.0)),
+        ], p=0.7),
+
+        albumentations.OneOf([
+            albumentations.OpticalDistortion(distort_limit=1.0),
+            albumentations.GridDistortion(num_steps=5, distort_limit=1.),
+            albumentations.ElasticTransform(alpha=3),
+        ], p=0.7),
+
+        albumentations.CLAHE(clip_limit=4.0, p=0.7),
+        albumentations.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.1, rotate_limit=15, border_mode=0, p=0.85),
+        albumentations.Resize(image_size, image_size),
+        albumentations.Cutout(max_h_size=int(image_size * 0.375), max_w_size=int(image_size * 0.375), num_holes=1, p=0.7)
+    ])
+
+    transforms_val = albumentations.Compose([
+        albumentations.Resize(image_size, image_size)
+        
+    ])
+
+    if not gray:
+        transforms_train = albumentations.Compose([
+            transforms_train,
+            albumentations.HueSaturationValue(hue_shift_limit=10, sat_shift_limit=20, val_shift_limit=10, p=0.5),
+            albumentations.Normalize()
+        ])
+        transforms_val = albumentations.Compose([
+            transforms_val,
+            albumentations.Normalize()
+        ])
+
+    return transforms_train, transforms_val
+
+temp_transform = albumentations.Compose([
+            # albumentations.Resize(224, 224),
+            albumentations.CenterCrop(224,224),
+            albumentations.Normalize()
+        ])
+
+
+class WindDataset(Dataset):
+    def __init__(self, image_list, target = None, test = False, transform = None, gray = False, a = False):
+        self.image_list = image_list
+        self.target = target
+        self.test = test
+        # self.transform = transform
+        self.transform = temp_transform
+        self.gray = gray
+        self.a = a
+    def __len__(self):
+        # if not self.a:        
+        #     return 8192
+        # else:
+        #     return 1024
+        return len(self.image_list)
+        # return 4096
+
+    def __getitem__(self, i):
+        if not self.gray:
+            image = cv2.imread(dataroot + self.image_list[i] + '.jpg')
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        else:
+            image = cv2.imread(dataroot + self.image_list[i] + '.jpg', 0)
+        if self.transform:
+            image = self.transform(image=image)['image'].astype(np.float32)
+        else:
+            image = image.astype(np.float32)
+        
+        if self.gray:
+            image = np.expand_dims(image, axis = 2)
+            image/=255.
+        # image = torch.Tensor(image).float()
+        image = torch.tensor(image).float()
+        image = image.permute(2,0,1)
+        if self.test:
+            return image
+        return image, torch.tensor(self.target[i]).float()
+
+def process_submiss(path):
+    image = cv2.imread(path)
+    image = temp_transform(image = image)['image'].astype(np.float32)
+    return image
---
+++ old_code/train.py
@@ -0,0 +1,156 @@
+import os
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+# import torch.optim as optim
+import torch_optimizer as optim
+from torch.optim import lr_scheduler, Adam, SGD
+from torch.utils.data.sampler import RandomSampler, SequentialSampler
+from dataset import get_transforms, WindDataset
+from models import Seresnext_Wind, SimpleModel, ResNetFromExample, \
+    Seresnext_Wind_DenseShallow_Swish, Seresnext_Wind_Conv2d_Swish, \
+        ResNetFromWeb
+from sklearn.model_selection import train_test_split
+
+import pickle
+from utils import train_epoch, val_epoch, RMSELoss
+
+
+
+class Hparameter(object):
+    def __init__(self):
+        self.batch_size = 1
+        self.lr = 2e-4
+        self.num_workers = 0
+        self.num_epochs = 30
+        # self.image_size = 368
+        self.image_size = 224
+        self.save_path = './weights/resnet_benhmark_change_m/'
+
+if __name__ == "__main__":
+    args = Hparameter()
+    device = torch.device('cuda')
+
+    # df = pd.read_csv('./data/training_set_labels.csv')
+    # target = np.array(df.wind_speed).astype(np.float32)
+    # max_wind = np.amax(target)
+    # target = list(target / max_wind)
+
+    # image_id = df.image_id.to_list()
+    # target = df.wind_speed.to_list()
+    
+
+    # train, val, y_train, y_val = train_test_split(image_id, target, test_size = 0.2, random_state = 42, shuffle = True)
+
+    df_train = pd.read_csv('./data/train_10first_10last.csv')
+    df_val = pd.read_csv('./data/val_10first_10last.csv')
+    train = df_train.image_id.to_list()
+    y_train = df_train.wind_speed.to_list()
+    # y_train = df_train.exp_wind.to_list()
+
+    val = df_val.image_id.to_list()
+    y_val = df_val.wind_speed.to_list()
+    # y_val = df_val.exp_wind.to_list()
+
+    # try:
+    #     mean = df_train.mean_wind.to_list()[0]
+    # except:
+    #     mean = None
+    mean = None
+
+
+    transforms_train, transforms_val = get_transforms(args.image_size, gray = True)
+
+    dataset_train = WindDataset(
+        image_list = train, 
+        target = y_train,
+        test = False, 
+        transform=transforms_train,
+        # gray = True
+        )
+    dataset_valid = WindDataset(
+        image_list = val,
+        target = y_val,
+        test = False, 
+        transform=transforms_val,
+        # gray = True,
+        a = True
+        )
+    train_loader = torch.utils.data.DataLoader(
+        dataset_train, 
+        batch_size=args.batch_size, 
+        # sampler=RandomSampler(dataset_train), 
+        shuffle = False,
+        num_workers=args.num_workers,
+        drop_last=True
+        )
+    valid_loader = torch.utils.data.DataLoader(
+        dataset_valid,
+        batch_size=args.batch_size, 
+        num_workers=args.num_workers,
+        shuffle=False,
+        drop_last=True
+        )
+
+    # model = Seresnet_Wind(type = 1, pretrained= True, gray = False)
+    # model = Seresnext_Wind_DenseShallow_Swish(type = 1, pretrained=True, gray = False)
+    # model = Seresnext_Wind_Conv2d_Swish(type = 1, pretrained=False, gray = False)
+    model = ResNetFromWeb()
+    # model = SimpleModel()
+    # model = ResNetFromExample()
+    # print(model)
+    # path = './weights/resnet50-full-Switch/epoch_17_4.67716.pth'
+    # path = './weights/seresnet50/epoch_10_0.18453.pth'
+    # path = './weights/seresnext50_exp/epoch_14_0.19151.pth'
+    # model.load_state_dict(torch.load(path))
+
+    # model = ResNet_Wind_LSTM(pretrained = False, gray = True)
+    model.to(device)
+
+
+    # real_batch = 64
+    # # acc_scale = args.batch_size / real_batch
+    # acc_scale = 1
+    # real_lr = args.lr*acc_scale
+
+    # optimizer = optim.RAdam(
+    #     model.parameters(),
+    #     lr= args.lr,
+    #     betas=(0.9, 0.999),
+    #     eps=1e-8,
+    #     weight_decay=0,
+    # )
+    # optimizer = SGD(model.parameters(), lr = args.lr, momentum=0.9, nesterov= True)
+    optimizer = Adam(model.parameters(), lr = args.lr)
+
+    criterion = RMSELoss()
+    # criterion = nn.MSELoss()
+    best_rmse = 30.
+    rmse = []
+    train_loss_overall = []
+    last_epoch = 0
+
+    for epoch in range(args.num_epochs):
+        model.train()
+        torch.cuda.synchronize()
+        train_loss = train_epoch(model, train_loader, optimizer, criterion, device)
+        model.eval()
+        torch.cuda.synchronize()
+        max_wind = 1
+        RMSE = val_epoch(model, valid_loader, criterion, device, max_wind, mean)
+        rmse.append(RMSE)
+        train_loss_overall.append(train_loss)
+        pick = {'train': train_loss_overall, 'val':rmse}
+        with open('./plot.pkl', 'wb') as f:
+            pickle.dump(pick, f)
+        # if RMSE < best_rmse or epoch%10 == 0:
+        name = args.save_path + 'epoch_%d_%.5f.pth'%(epoch + last_epoch + 1, RMSE)
+        # best_rmse = RMSE
+        print('Saving model...')
+        torch.save(model.state_dict(), name)
+    torch.save(model.state_dict(), args.save_path + 'last_epoch_%.5f.pth'%(RMSE))
