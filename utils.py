f5419fa3754e78f55395edc4f7b197902ab3999e
giang <lhgiang149@gmail.com>
2020-12-23 18:45:57 +0900

    update write_log, bug: HiddenPrints work out of with assumption

--- utils.py
+++ utils.py
@@ -1,132 +1,56 @@
 import torch
 import torch.nn as nn
-from tqdm import tqdm
+from torch.utils.data import Dataset
+
+import os, sys
 import numpy as np
-import time
 
-def train_epoch(model, loader, optimizer, criterion, device):
-
-    model.train()
-    train_loss = []
-    bar = tqdm(loader)
-    running_loss = 0
-    optimizer.zero_grad()
-
-    for i, (image, target) in enumerate(bar):
-    
-        image, target = image.to(device), target.to(device)
-        b_size = image.size()[0]
-        output = model(image)    
-        loss = criterion(output, target)        
-        optimizer.zero_grad(set_to_none = True)
-        loss.backward()
-        optimizer.step()
-
-        loss_np = loss.detach().cpu().item()
-        train_loss.append(loss_np)
-        # print(temp)
-        # if i%4==0:
-        temp1 = output.detach().cpu().numpy()# * 185.
-        temp2 = target.detach().cpu().numpy()# * 185.
-        # temp2 = np.expand_dims(temp2, axis = 1)
-        
-        # mean = 50.344008426206635
-        # temp1 = t2wind(mean, temp1)
-        # temp2 = t2wind(mean, temp2)
-
-        temp = np.concatenate((temp1,temp2), axis = 1)
-        print(temp)
-
-        bar.set_description('loss: %.5f' % (loss_np))
-    
-    # debug
-    # previous = model.head[12].weight.clone()
-    # previous = model.head[-2].weight.clone()
-    # for i, (image, target) in enumerate(bar):
-    #     image, target = image.to(device), target.to(device)
-    #     b_size = image.size()[0]
-    #     break
-
-    # while(True):
-    #     output = model(image)    
-    #     loss = criterion(output, target)
-    #     # now = model.model[0].weight
-    #     now = model.head[-2].weight
-    #     print('='*10)
-    #     print(previous,now)
-    #     if torch.sum(now-previous) == 0:
-    #         print('yes')
-    #     # print(model.head[11].running_mean)
-    #     # print(model.head[12].weight.grad)
-    #     optimizer.zero_grad()
-    #     # print(model.head[12].weight.grad)
-    #     loss.backward()
-    #     optimizer.step()
-
-    #     loss_np = loss.item()
-    #     train_loss.append(loss_np)
-    #     temp1 = output.detach().cpu().numpy() * 185.
-    #     temp2 = target.detach().cpu().numpy() * 185.
-    #     temp2 = np.expand_dims(temp2, axis = 1)
-    #     temp = np.concatenate((temp1,temp2), axis = 1)
-    #     print(temp)
-        # print(loss_np)
-        # bar.set_description('loss: %.5f' % (loss_np))
-    
-    
-    
-    train_loss = np.mean(train_loss)
-    print('running loss: %.5f' % (train_loss))
-    return train_loss
-
-def val_epoch(model, loader, criterion, device, max_value, mean):
-
-    model.eval()
-    RMSE = 0
-    num_sample = 0
-    bar = tqdm(loader)
-    with torch.no_grad():
-        for (image,target) in bar:
-            image, target = image.to(device), target.to(device)
-            num_sample += image.size()[0]
-            output = model(image).detach().cpu().numpy()
-            output*=max_value
-            # target = np.expand_dims(target.detach().cpu().numpy(), axis = 1)
-            target = target.detach().cpu().numpy()
-            target*=max_value
-            if mean is not None:
-                output = t2wind(mean, output)
-                target = t2wind(mean, target)
-            RMSE += np.sum((output - target)**2)
-            
-            temp = np.concatenate((output,target), axis = 1)
-            print(temp)
-        RMSE/=num_sample
-        RMSE = np.sqrt(RMSE)
-        
-        print('RMSE: %.5f' % (RMSE))
-    return RMSE
-
-def init_weights(m):
-    if isinstance(m, nn.Linear):
-        torch.nn.init.kaiming_normal_(m.weight)
-        m.bias.data.fill_(0.01)
-    elif isinstance(m, nn.Conv2d):
-        torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
-    elif isinstance(m, nn.BatchNorm2d):
-        m.reset_parameters()
-
-def config_momentum(m):
-    if isinstance(m, nn.BatchNorm2d):
-        m.momentum = 0.4
-            
 class RMSELoss(nn.Module):
     def __init__(self):
         super(RMSELoss, self).__init__()
         self.mse = nn.MSELoss()
+    def forward(self, x, y):
+        return torch.sqrt(self.mse(x, y))
 
-    def forward(self, output, target):
-        return torch.sqrt(self.mse(output, target))
+sigmoid = nn.Sigmoid()
 
-def t2wind(mean, t):
-    return mean * np.exp(t)
+class Swish(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, i):
+        result = i * sigmoid(i)
+        ctx.save_for_backward(i)
+        return result
+    @staticmethod
+    def backward(ctx, grad_output):
+        i = ctx.saved_variables[0]
+        sigmoid_i = sigmoid(i)
+        return grad_output * (sigmoid_i * (1 + i * (1 - sigmoid_i)))
+
+
+class Swish_Module(nn.Module):
+    def forward(self, x):
+        return Swish.apply(x)
+
+class HiddenPrints: # Block stdout in print function
+    def __enter__(self):
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout.close()
+        sys.stdout = self._original_stdout
+
+def write_log(save_path, model, optimizer, criterion, cache): # dump training information to txt file 
+    with HiddenPrints():
+        with open(save_path + 'model_info.txt', 'w') as f:
+            print(model, file = f)
+        with open(save_path + 'training_info.txt', 'w') as f:
+            f.write('Optimizer: \n')
+            print(optimizer, file = f)
+            f.write('Loss Function: \n')
+            print(criterion, file = f)
+
+            batch_size, image_size, shuffle = cache
+            f.write('Batch size: %d\n'%(batch_size))
+            f.write('Input image size: %d\n'%(image_size))
+            f.write('Data shuffle: '+str(shuffle) + '\n' )
