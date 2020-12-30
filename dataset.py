e0aa78151a7fe5d3fc76174848f56f05a14f4736
giang <lhgiang149@gmail.com>
2020-12-30 11:56:12 +0900

    add augmentation

--- dataset.py
+++ dataset.py
@@ -6,14 +6,35 @@ import numpy as np
 import albumentations
 from albumentations.pytorch.transforms import ToTensorV2
 
-def get_transform(image_size):
-    transform = albumentations.Compose([
-        albumentations.CenterCrop(image_size, image_size),
+def get_transform(image_size, base_size = 366):
+    if image_size > base_size:
+        resize = albumentations.Resize(image_size, image_size)
+    else:
+        resize = albumentations.CenterCrop(image_size, image_size)
+    
+    train_transform = albumentations.Compose([
+        albumentations.Transpose(p=0.3),
+        albumentations.VerticalFlip(p=0.3),
+        albumentations.HorizontalFlip(p=0.3),
+        albumentations.CLAHE(p=0.3),
+        albumentations.Equalize(p=0.3),
+        albumentations.OneOf([
+            albumentations.GaussianBlur(blur_limit=5),
+            albumentations.GaussNoise(var_limit = (5, 30)),
+            albumentations.MedianBlur(blur_limit = 5)
+        ], p = 0.5),
+        resize,
+        albumentations.Cutout(max_h_size = int(image_size * 0.2), max_w_size = int(image_size * 0.2), num_holes = 1, p =0.3),
+        albumentations.Normalize(), 
+        ToTensorV2()
+    ])
+    val_transform = albumentations.Compose([
+        resize,
         albumentations.Normalize(),
         ToTensorV2() # always use V2 follow this answer: https://albumentations.ai/docs/faq/#which-transformation-should-i-use-to-convert-a-numpy-array-with-an-image-or-a-mask-to-a-pytorch-tensor-totensor-or-totensorv2
     ]
     )
-    return transform
+    return train_transform, val_transform
 
 class WindDataset(Dataset):
     def __init__(self, image_list, target = None, transform = None, test = False):
@@ -23,8 +44,9 @@ class WindDataset(Dataset):
         self.test = test
 
     def __len__(self):
-        # return int(0.2*len(self.image_list))
+        # return int(0.1*len(self.image_list))
         return len(self.image_list)
+        # return 1024
 
     def __getitem__(self, i):
         # read by PIL and transform with pytorch in original 
