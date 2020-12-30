068e145ced781f5bfd042f3167e8dbdb59b16c51
giang <lhgiang149@gmail.com>
2020-12-30 11:56:49 +0900

    add EffB5

--- models.py
+++ models.py
@@ -3,12 +3,26 @@ import torch.nn as nn
 
 from torchvision import models
 import pretrainedmodels
+import geffnet
 
 from utils import Swish_Module
 
 '''
 Assume all with pretrained now which leads to all use Batch Norm and 3 channels image
 '''
+
+class ResNet50_BN_idea(nn.Module):
+    def __init__(self):
+        super(ResNet50_BN_idea, self).__init__()
+        self.model = models.resnet50(pretrained=True)
+        self.model.fc = nn.Sequential(
+            nn.Linear(2048, 100),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1),
+            nn.Linear(100,1)
+        )
+    def forward(self, x):
+        return self.model(x)
 
 class ResNetExample(nn.Module):
     def __init__(self):
@@ -62,7 +76,7 @@ class Seresnext_Wind(nn.Module):
 
 class Seresnext_Wind_Exp(nn.Module):
     def __init__(self, pretrained = True):
-        super(Seresnext_Wind, self).__init__()
+        super(Seresnext_Wind_Exp, self).__init__()
         if pretrained:
             self.extract = nn.Sequential(
                 *list(pretrainedmodels.__dict__["se_resnext101_32x4d"](num_classes=1000, pretrained="imagenet").children())[
@@ -78,13 +92,15 @@ class Seresnext_Wind_Exp(nn.Module):
 
         self.avg_pool = nn.AdaptiveAvgPool2d((1,1))
         self.head = nn.Sequential(
-            nn.Linear(2048, 512, bias= True),
-            Swish_Module(),
-            nn.BatchNorm1d(512),
+            nn.Linear(2048, 512, bias= False),
+            # Swish_Module(),
+            nn.LeakyReLU(inplace=True),
+            # nn.BatchNorm1d(512),
             nn.Dropout(p = 0.2),
-            nn.Linear(512, 128, bias= False),
+            nn.Linear(512, 128, bias= True),
+            nn.LeakyReLU(inplace=True),
             nn.Dropout(p = 0.2),
-            nn.Linear(128, 1)
+            nn.Linear(128, 1, bias = True)
         )
 
     def forward(self, x):
@@ -96,7 +112,7 @@ class Seresnext_Wind_Exp(nn.Module):
 
 class Seresnext_Wind_Exp_anchor(nn.Module):
     def __init__(self, pretrained = True):
-        super(Seresnext_Wind, self).__init__()
+        super(Seresnext_Wind_Exp_anchor, self).__init__()
         if pretrained:
             self.extract = nn.Sequential(
                 *list(pretrainedmodels.__dict__["se_resnext101_32x4d"](num_classes=1000, pretrained="imagenet").children())[
@@ -137,3 +153,23 @@ class Seresnext_Wind_Exp_anchor(nn.Module):
         x = x.view(x.size(0), -1)
         out = self.head(x)
         return out
+
+class Effnet_Wind_B5(nn.Module):
+    def __init__(self):
+        super(Effnet_Wind_B5, self).__init__()
+        self.model = geffnet.create_model('tf_efficientnet_b5_ns', pretrained=True)
+        self.model.classifier = nn.Sequential(
+            nn.Linear(2048, 512, bias= True),
+            nn.Dropout(p = 0.2),
+            nn.LeakyReLU(),
+            nn.Linear(512, 128, bias= True),
+            nn.Dropout(p = 0.2),
+            nn.LeakyReLU(),
+            nn.Linear(128, 32, bias= True),
+            nn.Dropout(p = 0.2),
+            nn.LeakyReLU(),
+            nn.Linear(32, 1, bias= True)
+        )
+    def forward(self, x):
+        return self.model(x)
+    
